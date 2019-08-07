0bdc080d2d81adf3063d06cde2a7cdfd0da34678
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-08-07 19:08:41 +0300

    PMM-3294 Use standard TLS config.

--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -15,6 +15,7 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/percona/exporter_shared"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
@@ -442,18 +443,7 @@ func main() {
 			w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 			w.Write(landingPage)
 		})
-		tlsCfg := &tls.Config{
-			MinVersion:               tls.VersionTLS12,
-			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
-			PreferServerCipherSuites: true,
-			CipherSuites: []uint16{
-				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
-				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-			},
-		}
-		srv.TLSConfig = tlsCfg
+		srv.TLSConfig = exporter_shared.TLSConfig()
 		srv.TLSNextProto = make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0)
 
 		log.Fatal(srv.ListenAndServeTLS(*sslCertFile, *sslKeyFile))
