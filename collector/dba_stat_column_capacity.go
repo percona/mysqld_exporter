d39b58835a3f929b3133319e98db5280321eced8
Oleksandr Korotych <korotych.o@soft2bet.com>
2023-11-08 11:52:38 +0200

    Add stat_column_capacity collector

---
+++ collector/dba_stat_column_capacity.go
@@ -0,0 +1,75 @@
+package collector
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/go-kit/log"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+const (
+	collector = "dba"
+
+	statColumnCapacityQuery = `
+		SELECT
+		   db_name table_schema,
+		   table_name,
+		   column_name,
+		   ratio capacity
+		FROM dba.stat_column_capacity
+`)
+
+var (
+	globalStatColumnCapacityDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, collector, "stat_column_capacity"),
+		"The current capacity of intager columns.",
+		[]string{"schema", "table", "column"}, nil,
+	)
+)
+
+type ScrapeStatColumnCapacityColumns struct{}
+
+// Name of the Scraper. Should be unique.
+func (ScrapeStatColumnCapacityColumns) Name() string {
+	return "current_value.columns"
+}
+
+// Help describes the role of the Scraper.
+func (ScrapeStatColumnCapacityColumns) Help() string {
+	return "Collect data from dba.stat_column_capacity"
+}
+
+// Version of MySQL from which scraper is available.
+func (ScrapeStatColumnCapacityColumns) Version() float64 {
+	return 5.1
+}
+
+// Scrape collects data from database connection and sends it over channel as prometheus metric.
+func (ScrapeStatColumnCapacityColumns) Scrape(ctx context.Context, db *sql.DB, ch chan<- prometheus.Metric, logger log.Logger) error {
+	columnCapacityRows, err := db.QueryContext(ctx, statColumnCapacityQuery)
+	if err != nil {
+		return err
+	}
+	defer columnCapacityRows.Close()
+
+	var (
+		schema, table, column string
+		value            float64
+	)
+
+	for columnCapacityRows.Next() {
+		if err := columnCapacityRows.Scan(
+			&schema, &table, &column, &value,
+		); err != nil {
+			return err
+		}
+		ch <- prometheus.MustNewConstMetric(
+			globalStatColumnCapacityDesc, prometheus.GaugeValue, value,
+			schema, table, column,
+		)
+	}
+	return nil
+}
+
+var _ Scraper = ScrapeStatColumnCapacityColumns{}
